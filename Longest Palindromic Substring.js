d47f25ec150b655c973cb9e4bf0dce880dfe3828
Holden Beale <itdbealh@aikencounty.net>
2018-09-26 15:05:42 -0400

    added explanation

--- Longest Palindromic Substring.js
+++ Longest Palindromic Substring.js
@@ -1,3 +1,7 @@
+//Given a string s, find the longest palindromic substring in s.
+//You may assume that the maximum length of s is 1000.
+
+
 /**
  * @param {string} s
  * @return {string}
@@ -95,7 +99,7 @@ var longestPalindrome = function(s) {
           }
    }
   }
-    
+
     if (isMonoPal == false){
        return pal;
    }else{
